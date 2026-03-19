9c192699386e7e8eb644c9e6385a1071bee97cad
kalkipiyadharshini <kalki@apeironengg.com>
2026-03-19 12:28:06 +0530

    changed the app/page.tsx

--- Components/common/statcard.tsx
+++ Components/common/statcard.tsx
@@ -4,16 +4,33 @@ interface StatCardProps {
 }
 
 export default function StatCard({ title, value }: StatCardProps) {
+
+  const colorStyles = {
+    "In Office": "bg-green-50 text-green-600",
+    "Not Checked-in": "bg-red-50 text-red-600",
+    "Late": "bg-yellow-50 text-yellow-600"
+  }
+
+  const badgeStyle =
+    colorStyles[title as keyof typeof colorStyles] || "bg-blue-50 text-blue-600"
+
   return (
-    <div className="p-5 bg-white rounded-2xl shadow-md hover:shadow-xl transition duration-300 border border-gray-100">
-      
-      {/* Title */}
-      <h2 className="text-sm text-gray-500 font-medium">
-        {title}
-      </h2>
+    <div className="p-5 bg-white rounded-2xl shadow-sm hover:shadow-md transition duration-300 border border-gray-100">
+
+      {/* Top Row */}
+      <div className="flex items-center justify-between">
+        <h2 className="text-sm text-gray-500 font-medium">
+          {title}
+        </h2>
+
+        {/* Small colored badge */}
+        <span className={`text-xs px-2 py-1 rounded-full ${badgeStyle}`}>
+          ●
+        </span>
+      </div>
 
       {/* Value */}
-      <p className="text-3xl font-bold text-gray-800 mt-2">
+      <p className="text-3xl font-semibold text-gray-800 mt-4">
         {value}
       </p>
 
